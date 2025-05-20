282fb99594e163e475ef39763256f9e95843fb3a
Mark John Lerry A. Casero <61694153+oxytoxin@users.noreply.github.com>
2025-05-20 11:51:27 +0800

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use ProductCategories;
+use App\Enums\ProductCategories;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
