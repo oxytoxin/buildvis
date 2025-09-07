93d202963cdbc1c715be689ca91c5c67fb270b3c
oxytoxin <oxytoxinsgrace@gmail.com>
2025-09-08 04:18:11 +0800

    component bug?

--- resources/views/components/application-logo.blade.php
+++
@@ -1,1 +0,0 @@
-<img class="h-28" src="{{ asset('images/logo-transparent.png') }}" alt="logo" />
--- resources/views/components/input-label.blade.php
+++
@@ -1,5 +0,0 @@
-@props(['value'])
-
-<label {{ $attributes->merge(['class' => 'block mb-1 text-sm font-medium']) }}>
-    {{ $value ?? $slot }}
-</label>
