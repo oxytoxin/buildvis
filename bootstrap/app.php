dab6243ae355d646d42724a2a6d0fbc8d1d835f8
oxytoxin <oxytoxinsgrace@gmail.com>
2025-09-08 05:48:59 +0800

    Revert "app.jsx bug?"
    This reverts commit f462234d0c7a00320f1eb76e0c79b2f807e3420b.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,7 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->web(append: [
-            HandleInertiaRequests::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
