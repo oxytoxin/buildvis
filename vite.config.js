ff6d42460a13e2b8daef5bdf585fd5ddcae75e3f
oxytoxin <oxytoxinsgrace@gmail.com>
2025-09-08 05:48:56 +0800

    Revert "component bug?"
    This reverts commit a9096081e4ac1026006d168d561db6d269f72181.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import {defineConfig} from "vite";
 import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
+import {wayfinder} from "@laravel/vite-plugin-wayfinder";
 import path from 'path';
 
 export default defineConfig({
     plugins: [
+        wayfinder(),
         laravel({
             input: [
                 "resources/css/app.css",
