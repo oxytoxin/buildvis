548d2cb120e4ec8a07cc5741ebe87502b8005eea
oxytoxin <oxytoxinsgrace@gmail.com>
2025-09-08 06:03:46 +0800

    panel bug fix

--- resources/css/filament/store/tailwind.config.js
+++ resources/css/filament/store/tailwind.config.js
@@ -1,22 +1,13 @@
-import defaultTheme from "tailwindcss/defaultTheme";
-import forms from "@tailwindcss/forms";
+import preset from "../../../../vendor/filament/filament/tailwind.config.preset";
 
-/** @type {import('tailwindcss').Config} */
 export default {
+    presets: [preset],
     content: [
-        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
-        "./storage/framework/views/*.php",
+        "./app/Filament/Store/**/*.php",
+        "./resources/views/filament/store/**/*.blade.php",
+        "./vendor/filament/**/*.blade.php",
         "./resources/views/**/*.blade.php",
+        "./storage/framework/views/*.php",
         "./resources/js/**/*.{js,jsx,ts,tsx}",
     ],
-
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
-
-    plugins: [forms],
 };
