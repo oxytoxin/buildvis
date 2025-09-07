2804c67b54a3c1d120c0140cd3eade94fff3b626
oxytoxin <oxytoxinsgrace@gmail.com>
2025-09-08 05:48:56 +0800

    Revert "component bug?"
    This reverts commit 93d202963cdbc1c715be689ca91c5c67fb270b3c.

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,1 @@
+<img class="h-28" src="{{ asset('images/logo-transparent.png') }}" alt="logo" />
---
+++ resources/views/components/text-input.blade.php
@@ -0,0 +1,3 @@
+@props(['disabled' => false])
+
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'w-full px-4 py-2 mb-2 outline-none ring-offset-0 focus:ring-2 focus:ring-lime-500 shadow rounded-lg']) }}>
